7f764526c42a1f54e3993799f299368d40614ad1
rlawltjd8547 <rlawltjd8547@nate.com>
2025-01-16 19:34:52 +0900

    #Git_Hub Acrtions실행

---
+++ board/src/main/java/com/main/board/mainBoard/Controller/MainBoardController.java
@@ -0,0 +1,12 @@
+package com.main.board.mainBoard.Controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/mainBoard")
+public class MainBoardController {
+
+
+}
